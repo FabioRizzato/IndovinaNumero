cd7ba64bfcc53ce28803f50917bfb1980661e7b3
FabioRizzato <rizzato.fabio96@gmail.com>
2015-10-16 20:43:22 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -18,7 +18,7 @@ public class Client {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         boolean indovinato = false;
-        int tentativo = 0;
+        int tentativo;
         String serverAddress = "127.0.0.1"; //indirizzo IP del server
         Socket s = new Socket(serverAddress, 9090); //creo il socket che si interfaccia con il server utilizzando la porta 9090
         BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));//stream server->client
@@ -26,18 +26,20 @@ public class Client {
         int tentativiDisponibili = Integer.parseInt(input.readLine());
         System.out.println("*BENVENUTO*");
         System.out.println("Indovina il numero pensato dal server.");
-        System.out.println("I numeri sono compresi tra 0 e 99, hai solo" + tentativiDisponibili + "tentativi");
+        System.out.println("I numeri sono compresi tra 0 e 99, hai solo " + tentativiDisponibili + " tentativi");
         for (int numTentativi = 1; !indovinato && numTentativi <= tentativiDisponibili; numTentativi++) {
             try {
-                System.out.print("Tentativo n." + numTentativi + ": ");
+                System.out.print("Tentativo n." + numTentativi + " : ");
                 tentativo = sc.nextInt();
                 output.println(tentativo); //manda al server il tentativo
-                String answer = input.readLine(); //sento la risposta del server, giusto o sbagliato?
+                System.out.println("mandato");
+                String answer = input.readLine(); //sento la risposta del server, giusto o sbagliato? maggiore o minore?
                 System.out.println(answer);
                 indovinato = Boolean.parseBoolean(new String(input.readLine())); //boolean per capire se continuare a provare o meno
                 } 
             catch (InputMismatchException e) {
                 System.out.println("Devi inserire un numero");
+                s.close();
                 break;
             }
         }
