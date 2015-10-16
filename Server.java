481d2b327b45f65fcb3b9c3f372facf891b8be5b
FabioRizzato <rizzato.fabio96@gmail.com>
2015-10-16 20:44:28 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -13,16 +13,12 @@ import java.util.Random;
  @author rizzato.fabio
  */
 public class Server {
-    static int tentativiDisponibili = 1;
+    static int tentativiDisponibili = 10;
     static boolean indovinato = false;
+    static Random r = new Random(new Date().getTime());
+
     
-    public static int GeneraRandom(int n) {
-        Random r = new Random(new Date().getTime());
-        int i = r.nextInt(n);
-        return i;
-    }
-    
-    public static void AnalizzaTentativo(/*PrintWriter out, */Socket s, int tent, int rand, int tentUtilizz) throws IOException {
+    public static void AnalizzaTentativo(Socket s, int tent, int rand, int tentUtilizz) throws IOException {
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
         if (tent == rand) {
             out.println("Hai Azzecato! Bravo!!");
@@ -42,18 +38,14 @@ public class Server {
             }
         }
     }
- 
-    
-    
-    
+
     
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(9090); //metto il server ad ascoltare sulla porta 9090
         try {
             while (true) {
                 Socket socket = listener.accept(); //accetto la connessione con il client
-                int n = 100;
-                int random = GeneraRandom(n);//genera numero random
+                int random = r.nextInt(100); //estrapola dall'oggetto Random un numero casuale in un range compreso tra 0 e (n-1)
                 int tentativiUtilizzati = 0;
                 System.out.println("Numero Generato: " + random);
                 try {
@@ -61,12 +53,17 @@ public class Server {
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //stream client->server
                     out.println(tentativiDisponibili);
                     indovinato = false;
-                    while (!indovinato) {
-                        int tentativo = Integer.parseInt(in.readLine()); //arriva il tentativo dal client
-                        tentativiUtilizzati++;
+                    int tentativo = 0;
+                    while (!indovinato&&tentativiUtilizzati<tentativiDisponibili) {
+                       	try { tentativo = Integer.parseInt(in.readLine()); }//arriva il tentativo dal client
+                        catch (NumberFormatException e){
+                            System.out.println("il client ha inviato un carattere anzichè un numero"); 
+                            break;
+                        }
+			tentativiUtilizzati++;
                         AnalizzaTentativo(socket, tentativo, random, tentativiUtilizzati);
                     }
-                } 
+                }
                 finally {
                     socket.close();
                 }
